1e9a1543257a6e7b5a61db718891cf10ddac4da7
xinobi-vm <rkarthik.vmc@gmail.com>
2024-02-09 20:11:24 -0500

    Made changes to the environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import getEnvModule from "./env";
-import dns from "dns";
-
-dns.setDefaultResultOrder("verbatim");
-
-getEnvModule().setEnvironmentVarsFromTestEnv(__dirname);
-
-process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
-console.log(`PROCESS ENV File: ${process.env}`);
-
-const env = {};
-
-// List of environment variables made available to the app
-["ISSUER", "CLIENT_ID", "STEPUP_LEVEL", "STEPUP_AGE"].forEach((key) => {
-  if (!process.env[key]) {
-    throw new Error(`Environment variable ${key} must be set. See README.md`);
-  }
-  env[key] = process.env[key];
-});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": env,
-  },
-  resolve: {
-    alias: {
-      "react-router-dom": path.resolve(
-        __dirname,
-        "node_modules/react-router-dom"
-      ),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
     },
-  },
-  server: {
-    port: process.env.PORT || 3000,
-  },
-  build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      // always throw with build warnings
-      onwarn(warning, warn) {
-        warn(
-          '\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.'
-        );
-        throw new Error(warning);
+    resolve: {
+      alias: {
+        "react-router-dom": path.resolve(
+          __dirname,
+          "node_modules/react-router-dom"
+        ),
       },
     },
-  },
+    server: {
+      port: process.env.PORT || 3000,
+    },
+    build: {
+      chunkSizeWarningLimit: 1600,
+      rollupOptions: {
+        // always throw with build warnings
+        onwarn(warning, warn) {
+          warn(
+            '\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.'
+          );
+          throw new Error(warning);
+        },
+      },
+    },
+  };
 });
